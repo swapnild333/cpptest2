29e59a59bbda454aed08c689665ad64ab49808f8
swapnilnci <swapnild333@gmail.com>
2023-04-23 16:41:52 +0530

    first commit

---
+++ insurance/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.contrib.auth.models import User
+from customer.models import Customer
+
+
+class admin(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+
+   
+    @property
+    def get_name(self):
+        return self.user.first_name+" "+self.user.last_name
+    @property
+    def get_instance(self):
+        return self
+    def __str__(self):
+        return self.user.first_name
+    
+class Category(models.Model):
+    category_name =models.CharField(max_length=20)
+    creation_date =models.DateField(auto_now=True)
+    def __str__(self):
+        return self.category_name
+
+class Policy(models.Model):
+    category= models.ForeignKey('Category', on_delete=models.CASCADE)
+    policy_name=models.CharField(max_length=200)
+    sum_assurance=models.PositiveIntegerField()
+    premium=models.PositiveIntegerField()
+    tenure=models.PositiveIntegerField()
+    creation_date =models.DateField(auto_now=True)
+    def __str__(self):
+        return self.policy_name
+
+class PolicyRecord(models.Model):
+    customer= models.ForeignKey(Customer, on_delete=models.CASCADE)
+    Policy= models.ForeignKey(Policy, on_delete=models.CASCADE)
+    status = models.CharField(max_length=100,default='Pending')
+    creation_date =models.DateField(auto_now=True)
+    def __str__(self):
+        return self.policy
+
+class Question(models.Model):
+    customer= models.ForeignKey(Customer, on_delete=models.CASCADE)
+    description =models.CharField(max_length=500)
+    admin_comment=models.CharField(max_length=200,default='Nothing')
+    asked_date =models.DateField(auto_now=True)
+    def __str__(self):
+        return self.description
